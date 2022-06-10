7ae7d4e338e9cb84cf6076de6fcd8a71f7bf174a
Fahad1993 <foso1015@gmail.com>
2022-06-10 10:39:30 +0300

    session

--- pages/home.js
+++ pages/home.js
@@ -1,17 +1,15 @@
 import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
 
 export default function Home() {
   const { data: session, status } = useSession()
-  const router = useRouter()
 
   if (status === 'loading') {
     return null
   }
-
-  if (session) {
-    router.push('/home')
-  }
-
-  return <a href='/api/auth/signin'>login</a>
+  
+  return (
+    <div>
+      {session ? <p>You are logged in!</p> : <p>You are not logged in 😞</p>}
+    </div>
+  )
 }

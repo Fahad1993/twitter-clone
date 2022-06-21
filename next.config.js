eeaa471c19ace410952a5f19e738f08e673e71d2
Fahad1993 <foso1015@gmail.com>
2022-06-21 21:30:04 +0300

    tweet, user faker, newReply, timeago, almost done.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    images: {
+      domains: ['cloudflare-ipfs.com', 'localhost'],
+    },
+  }
+
 
 module.exports = nextConfig
